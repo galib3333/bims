7a78576095b7912546c2dac268baba236ffb8e9f
Md. Asadullah Al Galib <asadullahgalib053@gmail.com>
2026-02-24 17:14:51 +0600

    vercel fix removed

--- vite.config.js
+++ vite.config.js
@@ -7,18 +7,12 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-            // Add this line to change output directory to dist
-            buildDirectory: 'dist'
         }),
         tailwindcss(),
     ],
-    // server: {
-    //     watch: {
-    //         ignored: ['**/storage/framework/views/**'],
-    //     },
-    // },
-    build: {
-        outDir: 'public/build',
-        emptyOutDir: true
-    }
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
 });
